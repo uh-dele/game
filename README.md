d34350e0b8e62aaa345485c08b7ee8168f338b12
Alison Abreu-Garcia <alison.iag@gmail.com>
2016-04-12 13:01:10 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Game
+
+[View assignment instructions](http://creativecomputing-s16.github.io/2015/04/12/games.html)
---
+++ sketch.js
@@ -0,0 +1,3 @@
+// Your game will go here
+
+// For performance reasons, use a small canvas size for your project (< 500 px)
